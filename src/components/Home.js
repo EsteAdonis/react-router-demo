473e48c5a7b56905f6f43e234e3b62abc3080c9e
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-10-28 01:27:32 -0600

    Navigating Programmatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 export const Home = () => {
-	return <div>Home page</div> 
+	const navigate = useNavigate();
+	return (
+		<>
+			<div>Home page</div> 
+			<button 
+				onClick={() => navigate('order-summary', {replace: true})}
+			>
+				Place Order
+			</button>
+		</>
+	)
 }
