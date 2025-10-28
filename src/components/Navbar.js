0f0f7508af4c89e210f071519433f21d6d5d5510
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-10-28 01:15:44 -0600

    Using NavLink and Applying Styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
+	const navLinkStyle = ({ isActive }) => {
+		return {
+			fontWeight: isActive ? 'bold' : 'normal',
+			TextDecoration: isActive ? 'none': 'underline'
+		}
+	}
 	return (
 		<nav>
-			<Link to="/">Home</Link>
-			<Link to="/about">About</Link>
+			<NavLink to="/" style={navLinkStyle}>Home</NavLink>
+			<NavLink to="/about" style={navLinkStyle}>About</NavLink>
 		</nav>
 	)
 }
