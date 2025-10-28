0b4c7aee15593b6eb742cd252aa1d9f1f0a00fb8
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-10-28 11:54:59 -0600

    Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { Products } from './components/Products';
 import { NoMatch } from './components/NoMatch';
 import { FeatureProduct } from './components/FeatureProduct';
 import { NewProduct } from './components/NewProduct';
+import { Users } from './components/Users';
+import { UserDetails } from './components/UserDetails';
+import { Admin } from './components/Admin';
 
 function App() {
   return (
@@ -22,6 +25,12 @@ function App() {
 					<Route path='featured' element={<FeatureProduct />} />
 					<Route path='new' element={<NewProduct />} />
 				</Route>
+				
+				<Route path='users' element={<Users />}>
+					<Route path=':userId' element={<UserDetails />}/>
+					<Route path='admin' element={<Admin />}/>
+				</Route>
+
 				<Route path="*" element={<NoMatch />} />
 			</Routes>
 		</>		
